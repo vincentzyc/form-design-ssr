289f6ffb41c0bd0c2d1f91431d713102f1381496
vincentzyc <vincentzyc@qq.com>
2021-07-04 00:51:14 +0800

    🎉init: init

--- server.js
+++ server.js
@@ -85,8 +85,8 @@ async function createServer(
 
 if (!isTest) {
   createServer().then(({ app }) =>
-    app.listen(3000, () => {
-      console.log('http://localhost:3000')
+    app.listen(3060, () => {
+      console.log('http://localhost:3060')
     })
   )
 }
