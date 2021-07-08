77b130b11e07710bf53e718816d677bd5640eb44
vincentzyc <vincentzyc@qq.com>
2021-07-08 14:01:47 +0800

    🎉init: init

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,18 +1,22 @@
 <template>
   <h1 @click="handClick()">{{ msg }}</h1>
+  <p>{{getres}}</p>
 </template>
 
 <script>
 import Axios from '../plugins/axios'
+import { ref } from 'vue'
 export default {
   async setup() {
+    let getres = ref('')
     const handClick = () => {
-      Axios.get('https://biz.trace.ickd.cn/auto/12312312').then(res => {
-        console.log(res);
+      Axios.get('https://biz.trace.ickd.cn/auto/1201594647434').then(res => {
+        getres.value = res.data
       })
     }
     return {
       handClick,
+      getres,
       msg: 'About'
     }
   }
