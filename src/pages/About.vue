5d2eaaacf69d7eb29b010cb2ccfdbbfc0515df98
vincentzyc <vincentzyc@qq.com>
2021-07-08 13:03:23 +0800

    🎉init: init

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,11 +1,18 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 @click="handClick()">{{ msg }}</h1>
 </template>
 
 <script>
+import Axios from '../plugins/axios'
 export default {
   async setup() {
+    const handClick = () => {
+      Axios.post('http://test-card.liulianglf.cn/service/jimPenn/page_id', { pid: "10086" }).then(res => {
+        console.log(res);
+      })
+    }
     return {
+      handClick,
       msg: 'About'
     }
   }
