2bec00df060822e3d5c3592b16fb4adb888d3699
vincentzyc <vincentzyc@qq.com>
2021-07-14 15:03:49 +0800

    🎉init: init

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,8 +1,11 @@
 import { createApp } from './main'
 
-const { app, router } = createApp()
+// const { app } = createApp()
+const { app } = createApp()
 
 // wait until router is ready before mounting to ensure hydration match
-router.isReady().then(() => {
-  app.mount('#app')
-})
+// router.isReady().then(() => {
+//   app.mount('#app')
+// })
+
+app.mount('#app')
