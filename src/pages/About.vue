0b273c35b6fd37672f917d2c8a2d40bc01c50ed6
vincentzyc <vincentzyc@qq.com>
2021-07-15 18:17:22 +0800

    🎉init: init

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,34 +7,25 @@
   <p>{{getres}}</p>
 </template>
 
-<script>
+<script setup>
 import Axios from '@/plugins/axios'
 import { ref, computed } from 'vue'
 import { Button } from 'vant';
 import { useStore } from '@/store'
-export default {
-  components: {
-    Button
-  },
-  async setup() {
-    const getres = ref('')
-    const kdnum = ref('')
-    const store = useStore()
-    const pageData = computed(() => store.state.pageData)
-    const handClick = () => {
-      store.commit('setPageData', { haha: '123123' });
-      console.log(pageData.value);
-      const num = kdnum.value || '1201594647434'
-      Axios.get('https://biz.trace.ickd.cn/auto/' + num).then(res => {
-        getres.value = res.data
-      })
-    }
-    return {
-      handClick,
-      getres,
-      kdnum,
-      msg: 'About'
-    }
-  }
+
+const msg = ref('About')
+const getres = ref('')
+const kdnum = ref('')
+const store = useStore()
+const pageData = computed(() => store.state.pageData)
+
+const handClick = () => {
+  store.commit('setPageData', { haha: '123123' });
+  console.log(pageData.value);
+  const num = kdnum.value || '1201594647434'
+  Axios.get('https://biz.trace.ickd.cn/auto/' + num).then(res => {
+    getres.value = res.data
+  })
 }
+
 </script>
