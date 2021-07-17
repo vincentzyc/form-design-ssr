23e1af11e43a94ecbc0a257ad18d9ed2bcdc43fc
vincentzyc <vincentzyc@qq.com>
2021-07-17 10:07:18 +0800

    🎉init: init

---
+++ src/composition/use-expose.ts
@@ -0,0 +1,12 @@
+import { getCurrentInstance } from 'vue';
+
+// expose public api
+export function useExpose(apis: Record<string, any>) {
+  const instance = getCurrentInstance();
+  if (instance) {
+    Object.assign(instance.proxy, apis);
+  }
+}
+
+// use
+// useExpose({ open, close })
