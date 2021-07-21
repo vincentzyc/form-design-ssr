5fc036116abae9fea1aaa4b3bf57556d2a1599ca
vincentzyc <vincentzyc@qq.com>
2021-07-21 09:28:33 +0800

    🎉init: init

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,27 @@
       <p>hahaha</p>
       <!-- <RenderPage /> -->
     </div>
-    <!-- <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>
-    <router-view v-slot="{ Component }">
-    <Suspense>
-    <component :is="Component" />
-    </Suspense>
-    </router-view>-->
-    <About />
+    <Home />
   </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, onMounted } from "vue"
-import About from "@/pages/About.vue"
+import Home from "@/pages/Home.vue"
 // import RenderPage from "@/components/RenderPage";
 import { useStore } from '@/store'
 // import { getSessionStorage, setSessionStorage } from "./utils/storage";
 // import { postMsgoOrigin, postMsgoUrl } from "./api";
+import '@/assets/css/main.css'
 import '@/assets/css/widget.styl';
 import '@/assets/css/themes.styl';
 import { formatStyle } from "./utils/format/unit";
-// import { initScript } from "./utils/dom";
+import { initScript } from "./utils/dom";
 import { defaultPageData } from "@/assets/js/page-data"
 
 export default defineComponent({
   components: {
-    About
+    Home
   },
   setup() {
     const store = useStore()
@@ -45,10 +39,13 @@ export default defineComponent({
     const initPage = (pgData: Record<string, any>) => {
       if (!pgData) return;
       store.commit('setPageData', pgData);
-      globalThis.appTitle = pgData.title
-      // document.title = pgData.title;
-      // initScript(pgData.statsCode, 'initjscode');  //添加第三方统计代码
+      globalThis.appTitle = pgData.title //设置页面标题
     }
+
+    onMounted(() => {
+      console.log(pageData.value);
+      initScript(pageData.value.statsCode, 'initjscode');  //添加第三方统计代码
+    })
 
     initPage(defaultPageData);
 
@@ -58,26 +55,3 @@ export default defineComponent({
   }
 })
 </script>
-
-
-<style>
-@font-face {
-  font-family: "Inter";
-  font-style: italic;
-  font-weight: 400;
-  font-display: swap;
-  src: url("./assets/fonts/Inter-Italic.woff2?#iefix") format("woff2"),
-    url("./assets/fonts/Inter-Italic.woff") format("woff");
-}
-.inter {
-  font-family: "Inter";
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
