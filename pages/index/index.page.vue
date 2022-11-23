045d6b51b4c876dd5cf41b81ce2a6501232f0101
vincentzyc <vincentzyc@qq.com>
2022-11-23 14:20:03 +0800

    🎉 init: init

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -1,26 +1,12 @@
 <template>
-  <h1>About</h1>
-  <p>Demo app using <code>vite-plugin-ssr</code>.</p>
-  <DragVerify :isPassing="isPassing" />
+  <h1>Home</h1>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
-import DragVerify from "@cp/DragVerify/DragVerify.vue";
-import { usePageContext } from "../../renderer/usePageContext";
+// import { usePageContext } from "../../renderer/usePageContext";
 
-const pageContext = usePageContext()
+// const pageContext = usePageContext()
 
-console.log(pageContext)
+// console.log(pageContext)
 
-const isPassing = ref(false);
 </script>
-
-<style>
-code {
-  font-family: monospace;
-  background-color: #eaeaea;
-  padding: 3px 5px;
-  border-radius: 4px;
-}
-</style>
