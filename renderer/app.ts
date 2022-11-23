8b4390d4b16dc7bda132f9a595f216bc32172734
vincentzyc <vincentzyc@qq.com>
2022-11-23 14:11:24 +0800

    🎉 init: init

--- renderer/app.ts
+++ renderer/app.ts
@@ -1,5 +1,4 @@
-import { createSSRApp, defineComponent, h } from 'vue'
-import PageShell from './PageShell.vue'
+import { createSSRApp, h } from 'vue'
 import { setPageContext } from './usePageContext'
 import type { PageContext } from './types'
 
@@ -7,21 +6,9 @@ export { createApp }
 
 function createApp(pageContext: PageContext) {
   const { Page, pageProps } = pageContext
-  const PageWithLayout = defineComponent({
-    render() {
-      return h(
-        PageShell,
-        {},
-        {
-          default() {
-            return h(Page, pageProps || {})
-          }
-        }
-      )
-    }
+  const app = createSSRApp({
+    render: () => h(Page, pageProps)
   })
-
-  const app = createSSRApp(PageWithLayout)
 
   // Make `pageContext` available from any Vue component
   setPageContext(app, pageContext)
