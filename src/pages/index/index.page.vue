ba29aab605017e7223863d6386fd1c0d00b08218
vincentzyc <vincentzyc@qq.com>
2022-11-24 10:50:57 +0800

    🎉 init: init

---
+++ src/pages/index/index.page.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Home</h1>
+</template>
+
+<script lang="ts" setup>
+import pgdata from '../../data/pgdata'
+
+console.log(pgdata)
+// import { usePageContext } from "../../renderer/usePageContext";
+
+// const pageContext = usePageContext()
+
+// console.log(pageContext)
+
+</script>
