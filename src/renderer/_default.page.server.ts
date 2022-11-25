e1eae67262897287a90d6df319f0f778ee2dde8a
vincentzyc <vincentzyc@qq.com>
2022-11-25 10:26:06 +0800

    🎉 init: init

--- src/renderer/_default.page.server.ts
+++ src/renderer/_default.page.server.ts
@@ -18,13 +18,14 @@ async function render(pageContext: PageContextServer) {
   const desc = (documentProps && documentProps.description) || 'App using Vite + vite-plugin-ssr'
 
   const documentHtml = escapeInject`<!DOCTYPE html>
-    <html lang="en">
+    <html lang="zh">
       <head>
         <meta charset="UTF-8" />
         <link rel="icon" href="${logoUrl}" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
+        <link rel="stylesheet" href="/src/renderer/base.css">
       </head>
       <body>
         <div id="app">${dangerouslySkipEscape(appHtml)}</div>
