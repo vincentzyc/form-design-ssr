b30c12306cba6aca23cb6abed9164d8dd93014c0
vincentzyc <vincentzyc@qq.com>
2022-12-09 09:36:03 +0800

    🎉 init: init

---
+++ src/components/RenderPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="widget-list" ref="widgetList">
+    <template v-for="(item, i) in wgList">
+      <WidgetItems :item="item" />
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import WidgetItems from "@cp/WidgetItems/index.vue";
+
+defineProps<{
+  wgList: Record<string, any>[];
+}>();
+</script>
