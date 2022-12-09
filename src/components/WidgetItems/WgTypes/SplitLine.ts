5293b2f8e0171f7b4c305d615f5e9d61ca5b6267
vincentzyc <vincentzyc@qq.com>
2022-12-09 09:36:54 +0800

    🎉 init: init

---
+++ src/components/WidgetItems/WgTypes/SplitLine.ts
@@ -0,0 +1,12 @@
+export interface TypesSplitLine {
+  key: string,
+  type: string, // "SplitLine",
+  name: string, // "分割线",
+  value: string, // "solid",
+  style: {
+    borderColor: string, // "#d3d3d3",
+    borderStyle: string, // "solid",
+    borderTopWidth: string, // "1px",
+    margin: string, // "20px 0px 20px 0px"
+  }
+}
