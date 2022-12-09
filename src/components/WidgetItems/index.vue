76b131eaa26fbc4aff091f67b285bf289aee4a1d
vincentzyc <vincentzyc@qq.com>
2022-12-09 09:36:14 +0800

    🎉 init: init

---
+++ src/components/WidgetItems/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="wgViewClass.value" :style="wgViewStyle.value">
+    <component :is="'Wg' + props.item.type" :item="props.item" :parentsWg="props.parentsWg" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import WgImgShow from "./WgImgShow.vue";
+import WgSplitLine from "./WgSplitLine.vue";
+
+export default defineComponent({
+  components: {
+    WgImgShow,
+    WgSplitLine,
+  },
+});
+</script>
+
+<script lang="ts" setup>
+interface Props {
+  item: Record<string, any>;
+  parentsWg?: Record<string, any>;
+}
+const props = withDefaults(defineProps<Props>(), {
+  item: () => ({}),
+  parentsWg: () => ({ type: "window" }),
+});
+
+const wgViewStyle = $computed(() => {
+  if (Array.isArray(props.item.list)) {
+    return {
+      ...props.item.style,
+      backgroundImage: `url(${props.item.backgroundImage})`,
+    };
+  }
+  return {};
+});
+const wgViewClass = $computed(() => (props.item.wgClassName ? props.item.wgClassName : "widget-view"));
+</script>
