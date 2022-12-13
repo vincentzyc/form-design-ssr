2d1b77a699b6c14fa155f783f8c9d8dfca84c8c9
vincentzyc <vincentzyc@qq.com>
2022-12-13 14:34:59 +0800

    🎉 init: init

--- src/components/WidgetItems/WgStaticText.vue
+++ src/components/WidgetItems/WgStaticText.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="wg-staticText clearfix" :style="domStyle">
-    <p v-html="changeRem(props.item.value)"></p>
-    <!-- <p :style="formatStyle(props.item.style)" v-html="changeRem(props.item.value)" @click="handleClick"></p> -->
-    <!-- <CustomPopup ref="elCustomPopup" :list="props.item.popupList || []" :show="props.item.showPopup" /> -->
+    <p :style="formatStyle(props.item.style)" @click="handleClick" v-html="html"></p>
+    <CustomPopup ref="elCustomPopup" :list="props.item.popupList || []" :show="props.item.showPopup" />
   </div>
 </template>
 <script lang="ts" setup>
 import { changeRem, formatStyle } from "@/utils/format/unit";
 import { jumpLink } from "@/utils/validate/link";
+import { onMounted } from "vue";
 import CustomPopup from "./WgPopup/index.vue";
 import { TypesStaticText } from "./WgTypes";
 
 const props = defineProps<{
   item: TypesStaticText;
 }>();
+
+let html = $ref("");
 
 const elCustomPopup = $ref<typeof CustomPopup>();
 const domStyle = {
@@ -26,4 +28,7 @@ const handleClick = () => {
     elCustomPopup && elCustomPopup.open();
   }
 };
+onMounted(() => {
+  html = changeRem(props.item.value);
+});
 </script>
