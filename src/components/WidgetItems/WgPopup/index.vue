35b8a647c7539b5acaeb217f3359c14fdd89a7ee
vincentzyc <vincentzyc@qq.com>
2022-12-13 14:35:08 +0800

    🎉 init: init

--- src/components/WidgetItems/WgPopup/index.vue
+++ src/components/WidgetItems/WgPopup/index.vue
@@ -1,20 +1,21 @@
 <template>
-  <van-popup v-model:show="props.show" class="custom-popup" style="max-width: 576px; overflow: visible">
+  <Popup v-model:show="props.show" class="custom-popup" style="max-width: 576px; overflow: visible">
     <svg class="close-icon" viewBox="0 0 1024 1024" @click="close">
       <path
         d="M512 1024a512 512 0 1 1 512-512 512 512 0 0 1-512 512z m0-981.333333a469.333333 469.333333 0 1 0 469.333333 469.333333A469.333333 469.333333 0 0 0 512 42.666667z m207.402667 676.437333a20.224 20.224 0 0 1-28.629334 0l-178.944-178.944-178.944 178.944a20.245333 20.245333 0 1 1-28.629333-28.629333l178.944-178.944-178.645333-178.645334a20.245333 20.245333 0 0 1 28.629333-28.629333l178.645333 178.645333 178.645334-178.645333a20.245333 20.245333 0 1 1 28.629333 28.629333l-178.645333 178.645334 178.944 178.944a20.224 20.224 0 0 1 0 28.629333z"
         fill="#ffffff"
       ></path>
     </svg>
-    <div class="popup-wrap"> <WidgetItems :item="item" v-for="item in props.list" />) </div>
-  </van-popup>
+    <div class="popup-wrap"> <WidgetItems :item="item" v-for="item in props.list" /> </div>
+  </Popup>
 </template>
 <script lang="ts" setup>
-import WidgetItems from "@/components/WidgetItems/index.vue";
-import "./css/style.styl";
+import WidgetItems from "@cp/WidgetItems/index.vue";
+import { Popup } from "vant";
+import "./css/style.css";
 
 interface Props {
-  list: [];
+  list: Record<string, any>[];
   show: boolean;
 }
 const props = withDefaults(defineProps<Props>(), {
