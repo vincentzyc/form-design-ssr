59a49539163140f3fb83868894462cb81c1bdad4
vincentzyc <vincentzyc@qq.com>
2022-12-14 09:40:57 +0800

    🎉 init: init

---
+++ src/components/WidgetItems/WgChildList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="childlist">
+    <WidgetItems :item="childItem" :parentsWg="props.item" v-for="childItem in item.list" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import WidgetItems from "@cp/WidgetItems/index.vue";
+
+const props = defineProps<{
+  item: Record<string, any>;
+}>();
+</script>
