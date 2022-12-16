41fe5fb63569c50e18b6842b01acee4fbba7408d
vincentzyc <vincentzyc@qq.com>
2022-12-16 10:16:34 +0800

    🎉 init: init

---
+++ src/components/WidgetItems/WgVideoPlay.vue
@@ -0,0 +1,34 @@
+<!-- import { formatStyle } from "@/utils/format/unit"
+import { defineComponent, PropType } from "vue"
+import { TypesVideoPlay } from "./WgTypes";
+
+export default defineComponent({
+  props: {
+    item: {
+      type: Object as PropType<TypesVideoPlay>,
+      required: true
+    }
+  },
+  setup(props) {
+    return () => (
+      <div class="wg-video-play" style={formatStyle(props.item.style)}>
+        <video {...props.item.videoAttr} style="width:100%;height:100%">您的浏览器不支持视频播放哦~</video>
+      </div>
+    )
+  }
+}) -->
+
+<template>
+  <div class="wg-video-play" :style="formatStyle(props.item.style)">
+    <video v-bind="props.item.videoAttr" style="width: 100%; height: 100%">您的浏览器不支持视频播放哦~</video>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { formatStyle } from "@/utils/format/unit";
+import { TypesVideoPlay } from "./WgTypes";
+
+const props = defineProps<{
+  item: TypesVideoPlay;
+}>();
+</script>
