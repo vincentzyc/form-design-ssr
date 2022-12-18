ca5ed4d9e263ca541d7edf4c0d6bc5ff05079e91
vincentzyc <vincentzyc@qq.com>
2022-12-18 16:25:25 +0800

    🎉 init: init

---
+++ src/components/WidgetItems/WgTypes/StaticText.ts
@@ -0,0 +1,14 @@
+export interface TypesStaticText {
+  key: string,
+  type: string, // StaticText
+  name: string, // 文本描述
+  value: string, // 这是一段文本
+  link: string, // ""
+  popupList: Record<any, any>[], // []
+  showPopup: false, // false
+  backgroundImage: string, // ""
+  backgroundColor: string, // ""
+  style: {
+    margin: string // 0px 0px 0px 0px
+  }
+}
