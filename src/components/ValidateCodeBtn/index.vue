455189bde6bdca7e1c32deeae0612c49e4bfb6a2
vincentzyc <vincentzyc@qq.com>
2023-01-03 10:25:54 +0800

    🎉 init: init

--- src/components/ValidateCodeBtn/index.vue
+++ src/components/ValidateCodeBtn/index.vue
@@ -1,17 +1,17 @@
 <template>
-  <button class="validate-code-btn" :disabled="disbtn" @click="getCode">{{codetxt time}}</button>
+  <button class="validate-code-btn" :disabled="disbtn" @click="getCode">获取验证码{{time}}</button>
 </template>
 
 <script lang="ts" setup>
 import "./style.css";
 import { showToast, showLoadingToast, closeToast } from "vant";
 
-showLoadingToast({
-  message: "加载中...",
-  forbidClick: true,
-});
+// showLoadingToast({
+//   message: "加载中...",
+//   forbidClick: true,
+// });
 
-// import { getVerifyCode } from '@/api';
+// // import { getVerifyCode } from '@/api';
 
 const props = defineProps<{
   phone: string;
@@ -21,12 +21,12 @@ let codetxt = $ref("获取验证码");
 let time = $ref<string | number>();
 let disbtn = $ref(false);
 
-/**
- * 倒计时
- * @param {Number} time  倒计时秒数
- * @param {Function} tickFunc  每秒回调
- * @param {Function} done     结束后回调
- */
+// /**
+//  * 倒计时
+//  * @param {Number} time  倒计时秒数
+//  * @param {Function} tickFunc  每秒回调
+//  * @param {Function} done     结束后回调
+//  */
 const countDown = (time: number, tickFunc: (time: number) => void, done: () => void) => {
   const tick = () => {
     setTimeout(() => {
