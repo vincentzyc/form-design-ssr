4e643ca72d3e25714fc6ec73a5f7f6405afd9f61
vincentzyc <vincentzyc@qq.com>
2023-01-03 14:54:32 +0800

    🎉 init: init

--- src/pages/index/index.page.vue
+++ src/pages/index/index.page.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 @click="test">Home</h1>
+  <h1>Home</h1>
   <div>
     <RenderPage :wgList="wgList" />
   </div>
@@ -9,16 +9,9 @@
 // import { reactive } from 'vue';
 import RenderPage from "@cp/RenderPage.vue";
 import widgetsData from "@/data/pgdata";
-import { showLoadingToast } from "vant";
 
 const wgList = $ref(widgetsData.list);
-
-function test() {
-  showLoadingToast({
-    message: "正在发送...",
-    forbidClick: true,
-  });
-}
+// const item  = reactive({"type":"ImgShow","name":"图片展示","positionFixed":"auto","fixedTypes":["auto","top","bottom"],"scrollHeight":0,"styleType":"col1","styleTypes":[{"value":"col1","label":"单列"},{"value":"col2","label":"双列"}],"imglist":[{"img":"https://static.jetmobo.com/image/content-h5/20221125/1669333983504.jpg","link":""}],"style":{"margin":"0px 0px 0px 0px"},"key":"ImgShow_1597832935582_347781"})
 
 // import pgdata from '../../data/pgdata'
 
