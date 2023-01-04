3fb408c1b18b29cf1e5dd8800eee2018f159d873
vincentzyc <vincentzyc@qq.com>
2023-01-04 09:21:11 +0800

    🎉 init: init

--- src/components/WidgetItems/WgChildList.vue
+++ src/components/WidgetItems/WgChildList.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="childlist">
+  <div class="childlist" :style="formatStyle(item.style)">
     <WidgetItems :item="childItem" :parentsWg="props.item" v-for="childItem in item.list" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { formatStyle } from "@/utils/format/unit";
 import WidgetItems from "@cp/WidgetItems/index.vue";
 
 const props = defineProps<{
