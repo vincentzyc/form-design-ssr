64d219a28d039ecfdd361ce5f9ecb1ffffc0399a
vincentzyc <vincentzyc@qq.com>
2023-01-06 10:07:39 +0800

    🎉 init: init

--- src/pages/index/index.page.vue
+++ src/pages/index/index.page.vue
@@ -5,25 +5,36 @@
 </template>
 
 <script lang="ts">
-// export { getDocumentProps }
-
-// function getDocumentProps() {
-//   return {
-//     title: 'About SpaceX',
-//     description: 'Our mission is to explore the galaxy.'
-//   }
+import widgetsData from "@/data/pgdata";
+// 静态设置
+// export const documentProps = {
+//   title: widgetsData.title,
+//   description: '这是自定义的页面描述'
 // }
 
-export const documentProps = {
-  title: '自定义标题',
-  description: '这是自定义的页面描述'
+// 动态设置
+
+async function getDocumentProps() {
+  await lazy()
+  return {
+    title: widgetsData.title,
+    description: '这是自定义的页面描述'
+  }
 }
+async function lazy(){
+  return new Promise((resolve)=>{
+    setTimeout(() => {
+      resolve(true)
+    }, 100);
+  })
+}
+export { getDocumentProps }
+
 </script>
 
 <script lang="ts" setup>
 import { formatStyle } from "@/utils/format/unit";
 import RenderPage from "@cp/RenderPage.vue";
-import widgetsData from "@/data/pgdata";
 
 const wgList = $ref(widgetsData.list);
 
