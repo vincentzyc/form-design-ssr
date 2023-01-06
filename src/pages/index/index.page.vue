8a0bf62299a9b57c3cc9daa1893fafd05bda6546
vincentzyc <vincentzyc@qq.com>
2023-01-06 09:48:00 +0800

    🎉 init: init

--- src/pages/index/index.page.vue
+++ src/pages/index/index.page.vue
@@ -1,19 +1,31 @@
 <template>
-  <h1>Home</h1>
-  <div>
+  <div class="wrapper" :style="formatStyle(widgetsData.style)">
     <RenderPage :wgList="wgList" />
   </div>
 </template>
 
+<script lang="ts">
+// export { getDocumentProps }
+
+// function getDocumentProps() {
+//   return {
+//     title: 'About SpaceX',
+//     description: 'Our mission is to explore the galaxy.'
+//   }
+// }
+
+export const documentProps = {
+  title: '自定义标题',
+  description: '这是自定义的页面描述'
+}
+</script>
+
 <script lang="ts" setup>
-// import { reactive } from 'vue';
+import { formatStyle } from "@/utils/format/unit";
 import RenderPage from "@cp/RenderPage.vue";
 import widgetsData from "@/data/pgdata";
 
 const wgList = $ref(widgetsData.list);
-// const item  = reactive({"type":"ImgShow","name":"图片展示","positionFixed":"auto","fixedTypes":["auto","top","bottom"],"scrollHeight":0,"styleType":"col1","styleTypes":[{"value":"col1","label":"单列"},{"value":"col2","label":"双列"}],"imglist":[{"img":"https://static.jetmobo.com/image/content-h5/20221125/1669333983504.jpg","link":""}],"style":{"margin":"0px 0px 0px 0px"},"key":"ImgShow_1597832935582_347781"})
-
-// import pgdata from '../../data/pgdata'
 
 // console.log(pgdata)
 // import { usePageContext } from "../../renderer/usePageContext";
