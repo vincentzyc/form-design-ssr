661e3b06613c5ab4a77a4b267c052007dab4b987
vincentzyc <vincentzyc@qq.com>
2023-01-09 10:50:40 +0800

    🎉 init: init

--- src/renderer/app.ts
+++ src/renderer/app.ts
@@ -2,6 +2,9 @@ import { createSSRApp, h } from 'vue'
 import { setPageContext } from './usePageContext'
 import type { PageContext } from './types'
 import VueDOMPurifyHTML from 'vue-dompurify-html';
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
 
 export { createApp }
 
@@ -10,6 +13,7 @@ function createApp(pageContext: PageContext) {
   const app = createSSRApp({
     render: () => h(Page, pageProps)
   })
+  app.use(pinia)
   app.use(VueDOMPurifyHTML);
 
   // Make `pageContext` available from any Vue component
