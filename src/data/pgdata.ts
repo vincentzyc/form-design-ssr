90d03dfb4868757c9a7fa4631030279d8cad177d
vincentzyc <vincentzyc@qq.com>
2023-02-08 09:05:24 +0800

    🎉 init: init

--- src/data/pgdata.ts
+++ src/data/pgdata.ts
@@ -260,21 +260,6 @@ export default {
       key: "ImgShow_1597832935582_347666",
     },
     {
-      type: "ImgShow",
-      name: "图片展示",
-      positionFixed: "auto",
-      fixedTypes: ["auto", "top", "bottom"],
-      scrollHeight: 0,
-      styleType: "col1",
-      styleTypes: [
-        { value: "col1", label: "单列" },
-        { value: "col2", label: "双列" },
-      ],
-      imglist: [{ img: "https://static.jetmobo.com/image/content-h5/20221125/1669333983504.jpg", link: "" }],
-      style: { margin: "0px 0px 0px 0px" },
-      key: "ImgShow_1597832935582_347777",
-    },
-    {
       type: "Button",
       name: "按钮",
       btnText: "提交",
