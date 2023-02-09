bd2af41de6771bef3cf2c1f379d204ff68e78e30
vincentzyc <vincentzyc@qq.com>
2023-02-09 10:56:04 +0800

    🎉 init: init

--- src/components/WidgetItems/WgDatePicker.vue
+++ src/components/WidgetItems/WgDatePicker.vue
@@ -1,76 +1,76 @@
 <template>
-<div>
-        <div
-          class={wrapClass}
-          style={formatStyle(wgData.style)}
-        >
-          <div
-            class="wg-title"
-            v-show={wgData.showLabel}
-            style={{ width: changeRem(wgData.label.labelWidth) }}
-          >{wgData.label.labelTitle}</div>
-          <div class="flex-auto disabled-input" onClick={open}>
-            <input
-              disabled
-              id={wgData.key}
-              v-model={formData[wgData.apiKey]}
-              placeholder={wgData.placeholder}
-              class="wg-input" />
-          </div>
-        </div>
-        <Popup position="bottom" v-model={[showPicker.value, 'show']} teleport="body">
-          <DatePicker
-            v-model={currentDate.value}
-            type="date"
-            title="选择年月日"
-            min-date={minDate}
-            max-date={maxDate}
-            onCancel={cancel}
-            onConfirm={confirm}
-          />
-        </Popup>
+  <div>
+    <div :class="wrapClass" :style="formatStyle(wgData.style)">
+      <div class="wg-title" v-show="wgData.showLabel" :style="{ width: changeRem(wgData.label.labelWidth) }">{{
+        wgData.label.labelTitle
+      }}</div>
+      <div class="flex-auto disabled-input" @click="open">
+        <input
+          disabled
+          :id="wgData.key"
+          v-model="formData[wgData.apiKey]"
+          :placeholder="wgData.placeholder"
+          class="wg-input"
+        />
       </div>
+    </div>
+    <Popup position="bottom" v-model.show="showPicker" teleport="body">
+      <DatePicker
+        v-model="currentDate"
+        type="date"
+        title="选择年月日"
+        :min-date="minDate"
+        :max-date="maxDate"
+        :onCancel="cancel"
+        :onConfirm="confirm"
+      />
+    </Popup>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { defineComponent, ref, PropType } from 'vue';
-import { DatePicker } from 'vant';
+import { DatePicker } from "vant";
 import { changeRem, formatStyle } from "@/utils/format/unit";
-import { useWgFormList } from '@/composition/use-wgform';
+import { useWgFormList } from "@/composition/use-wgform";
 import { TypesDatePicker } from "./WgTypes";
 
 const props = defineProps<{
   item: TypesDatePicker;
 }>();
 
-const wgFormList = useWgFormList()
-    const { wgData, formData } = wgFormList.useAddForm(props.item)
+const wgFormList = useWgFormList();
+const { wgData, formData } = wgFormList.useAddForm(props.item);
 
-    const minDate = new Date(1920, 0, 1)
-    const maxDate = new Date(2120, 12, 31)
+const minDate = new Date(1920, 0, 1);
+const maxDate = new Date(2120, 12, 31);
 
-    const showPicker = ref(false)
-    const currentDate = ref(new Date())
+const currentTime = new Date();
+const currentYear = currentTime.getFullYear().toString();
+const currentMonth = (currentTime.getMonth() + 1).toString();
+const currentDay = currentTime.getDate().toString();
 
-    const formatDate = (Date: Date) => {
-      const year = Date.getFullYear(),
-        month = Date.getMonth() + 1,
-        date = Date.getDate();
-      return `${year}-${month < 10 ? '0' + month : month}-${date < 10 ? '0' + date : date}`
-    }
+let showPicker = $ref(false);
+const currentDate = $ref([currentYear, currentMonth, currentDay]);
 
-    const open = () => showPicker.value = true;
+const formatDate = (Date: Date) => {
+  const year = Date.getFullYear(),
+    month = Date.getMonth() + 1,
+    date = Date.getDate();
+  return `${year}-${month < 10 ? "0" + month : month}-${date < 10 ? "0" + date : date}`;
+};
 
-    const close = () => showPicker.value = false;
+const open = () => (showPicker = true);
 
-    const cancel = () => {
-      close()
-    }
+const close = () => (showPicker = false);
 
-    const confirm = (value: Date) => {
-      formData[wgData.apiKey] = formatDate(value);
-      close()
-    }
+const cancel = () => {
+  close();
+};
 
-    const wrapClass = ['wg-item', wgData.label.labelPosition === 'top' ? 'flex-column' : 'align-middle']
+const confirm = (value: Date) => {
+  formData[wgData.apiKey] = formatDate(value);
+  close();
+};
+
+const wrapClass = ["wg-item", wgData.label.labelPosition === "top" ? "flex-column" : "align-middle"];
 </script>
