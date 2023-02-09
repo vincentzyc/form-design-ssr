259b6a6573f314a1553c123ecf8bb95b1a619e3d
vincentzyc <vincentzyc@qq.com>
2023-02-09 14:24:01 +0800

    🎉 init: init

--- src/components/WidgetItems/WgDatePicker.vue
+++ src/components/WidgetItems/WgDatePicker.vue
@@ -14,22 +14,22 @@
         />
       </div>
     </div>
-    <Popup position="bottom" v-model.show="showPicker" teleport="body">
+    <Popup position="bottom" v-model:show="showPicker" teleport="body">
       <DatePicker
         v-model="currentDate"
         type="date"
         title="选择年月日"
         :min-date="minDate"
         :max-date="maxDate"
-        :onCancel="cancel"
-        :onConfirm="confirm"
+        @cancel="cancel"
+        @confirm="confirm"
       />
     </Popup>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { DatePicker } from "vant";
+import { Popup, DatePicker } from "vant";
 import { changeRem, formatStyle } from "@/utils/format/unit";
 import { useWgFormList } from "@/composition/use-wgform";
 import { TypesDatePicker } from "./WgTypes";
@@ -45,19 +45,16 @@ const minDate = new Date(1920, 0, 1);
 const maxDate = new Date(2120, 12, 31);
 
 const currentTime = new Date();
-const currentYear = currentTime.getFullYear().toString();
-const currentMonth = (currentTime.getMonth() + 1).toString();
-const currentDay = currentTime.getDate().toString();
+const currentYear = currentTime.getFullYear();
+const currentMonth = currentTime.getMonth() + 1;
+const currentDay = currentTime.getDate();
 
 let showPicker = $ref(false);
-const currentDate = $ref([currentYear, currentMonth, currentDay]);
-
-const formatDate = (Date: Date) => {
-  const year = Date.getFullYear(),
-    month = Date.getMonth() + 1,
-    date = Date.getDate();
-  return `${year}-${month < 10 ? "0" + month : month}-${date < 10 ? "0" + date : date}`;
-};
+const currentDate = $ref([
+  currentYear.toString(),
+  (currentMonth < 10 ? "0" + currentMonth : currentMonth).toString(),
+  (currentDay < 10 ? "0" + currentDay : currentDay).toString(),
+]);
 
 const open = () => (showPicker = true);
 
@@ -67,8 +64,8 @@ const cancel = () => {
   close();
 };
 
-const confirm = (value: Date) => {
-  formData[wgData.apiKey] = formatDate(value);
+const confirm = (value: { selectedValues: string[] }) => {
+  formData[wgData.apiKey] = value.selectedValues.join("-");
   close();
 };
 
