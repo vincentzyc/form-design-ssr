e81d717758fdb50723d385ddbfd58ab2f8a23c2d
vincentzyc <vincentzyc@qq.com>
2023-02-11 16:08:48 +0800

    🎉 init: init

--- src/pages/index/index.page.vue
+++ src/pages/index/index.page.vue
@@ -1,11 +1,48 @@
 <template>
-  <div class="wrapper" :style="formatStyle(widgetsData.style)">
+  <div class="wrapper" :style="formatStyle(widgetsData && widgetsData.style)">
     <RenderPage :wgList="wgList" />
+    <!-- <p>测试服务端获取页面数据</p> -->
   </div>
 </template>
 
 <script lang="ts">
-import widgetsData from "@/data/pgdata";
+// import widgetsData from "@/data/pgdata";
+import { formatStyle } from "@/utils/format/unit";
+import RenderPage from "@cp/RenderPage.vue";
+import { defineComponent } from "vue";
+import { usePageContext } from "../../renderer/usePageContext";
+
+export default defineComponent({
+  components: { RenderPage },
+  setup() {
+    const pageContext = usePageContext();
+    
+    const pageProps = pageContext.pageProps;
+
+    let widgetsData = $ref<Record<string, any> | null>(null);
+    let wgList = $ref<Record<string, any>[]>([]);
+
+    if (pageProps) widgetsData = pageProps.pgData;
+    if (widgetsData) wgList = widgetsData.list;
+
+    // async function getPageData() {
+    //   // await lazy(300);
+    //   const pgdata = await import("@/data/pgdata");
+    //   if (pgdata) widgetsData = pgdata.default || pgdata;
+    //   if (widgetsData) wgList = widgetsData.list;
+    // }
+
+    // await getPageData();
+
+    // console.log(wgList);
+
+    return {
+      widgetsData,
+      wgList,
+      formatStyle,
+    };
+  },
+});
 // 静态设置
 // export const documentProps = {
 //   title: widgetsData.title,
@@ -14,37 +51,19 @@ import widgetsData from "@/data/pgdata";
 
 // 动态设置
 
-async function getDocumentProps() {
-  await lazy();
-  return {
-    title: widgetsData.title,
-    description: "这是自定义的页面描述",
-  };
-}
-async function lazy(time=100) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(true);
-    }, time);
-  });
-}
-export { getDocumentProps };
-</script>
-
-<script lang="ts" setup>
-import { formatStyle } from "@/utils/format/unit";
-import RenderPage from "@cp/RenderPage.vue";
-
-let wgList = $ref<typeof widgetsData.list>([]);
-async function getPageData() {
-  await lazy(300);
-  wgList = widgetsData.list;
-}
-getPageData()
-// console.log(pgdata)
-// import { usePageContext } from "../../renderer/usePageContext";
-
-// const pageContext = usePageContext()
-
-// console.log(pageContext)
+// async function getDocumentProps() {
+//   await lazy();
+//   return {
+//     title: widgetsData.title,
+//     description: "这是自定义的页面描述",
+//   };
+// }
+// async function lazy(time = 100) {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(true);
+//     }, time);
+//   });
+// }
+// export { getDocumentProps };
 </script>
