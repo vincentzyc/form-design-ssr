7876b62ab10eb5a573d7cce54e17a14484bb307f
vincentzyc <vincentzyc@qq.com>
2023-08-25 09:34:39 +0800

    🎉 init: init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  // devtools: { enabled: true },
+  devServer: {
+    port: 3010,
+  },
+  css: ['~/assets/css/base.css', '~/assets/css/main.less'],
+  modules: ['@vant/nuxt', '@pinia/nuxt'],
+  vant: {
+    lazyload: true,
+  },
+});
