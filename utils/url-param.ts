c8a279e830386b978e41e52fdb034c1b52a5979d
vincentzyc <vincentzyc@qq.com>
2023-09-02 00:27:17 +0800

    🎉init: init

--- utils/url-param.ts
+++ utils/url-param.ts
@@ -3,41 +3,33 @@ type UrlParamBack = null | string | Record<string, any>;
 /**
  * 获取url参数值
  * @param {String} name 参数名称(不传则返回一个全部参数对象)
+ * @param {String} newUrl 要处理的链接
  */
 export function getUrlParam(name: string = '', newUrl: string): UrlParamBack {
-  let i = 0;
   let parameterName = '';
   let parameterValue = '';
-  let paramObj: Record<string, any> = {};
   const str = newUrl || window.location.href;
+  const paramObj: { [key: string]: string } = {};
   const strLength = str.length;
-  while (i < strLength) {
-    const c = str.charAt(i);
-    if (i === strLength - 1) {
-      let startValue = str.lastIndexOf('=', i);
-      if (parameterName && startValue > 0) {
-        parameterValue = str.slice(startValue + 1, i + 1);
-        paramObj[parameterName] = parameterValue;
-        parameterName = '';
-      }
-      i++;
-    } else if (c === '{') {
+
+  for (let i = 0; i < strLength; i++) {
+    const c = str[i];
+
+    if (c === '{') {
       if (parameterName) {
         const closingBraceIndex = findClosingBrace(str, i);
+        if (closingBraceIndex < i) continue;
         const newIndex = closingBraceIndex + 1;
         parameterValue = str.slice(i, newIndex);
         paramObj[parameterName] = parameterValue;
         parameterName = '';
         i = newIndex;
-      } else {
-        i++;
       }
     } else if (c === '=') {
       let startParameter = str.lastIndexOf('&', i);
       if (startParameter < 0) startParameter = str.lastIndexOf('?', i);
       if (startParameter < 0) startParameter = str.lastIndexOf('#', i);
       parameterName = str.slice(startParameter + 1, i);
-      i++;
     } else if (c === '&') {
       let startValue = str.lastIndexOf('=', i);
       if (parameterName && startValue > 0) {
@@ -45,12 +37,15 @@ export function getUrlParam(name: string = '', newUrl: string): UrlParamBack {
         paramObj[parameterName] = parameterValue;
         parameterName = '';
       }
-      i++;
-    } else {
-      i++;
+    }
+
+    if (i === strLength - 1 && parameterName) {
+      parameterValue = str.slice(str.lastIndexOf('=', i) + 1);
+      paramObj[parameterName] = parameterValue;
     }
   }
-  if (name) return paramObj[name] || '';
+
+  if (name) return paramObj[name];
   return paramObj;
 }
 /**
@@ -59,17 +54,28 @@ export function getUrlParam(name: string = '', newUrl: string): UrlParamBack {
  * @param {number} startIndex - 开始搜索的索引。
  * @return {number} - 闭合括号的索引。
  */
-export function findClosingBrace(str: string, startIndex: number): number {
+function findClosingBrace(str: string, startIndex: number): number {
+  let i;
   let braceDepth = 1;
-  let i = startIndex + 1;
-  while (i < str.length && braceDepth > 0) {
-    const innerChar = str.charAt(i);
-    if (innerChar === '{') {
-      braceDepth++;
-    } else if (innerChar === '}') {
-      braceDepth--;
+  const strLength = str.length;
+
+  for (i = startIndex + 1; i < strLength && braceDepth > 0; ++i) {
+    const innerChar = str[i];
+    switch (innerChar) {
+      case '{':
+        ++braceDepth;
+        break;
+      case '}':
+        --braceDepth;
+        break;
     }
-    i++;
   }
+
   return i - 1;
 }
+
+const url = `http://test-cms.jetmobo.com/h5/index.html#haha?id=8999357689071467516&abtest=1&pid=48075&ogpid=48075&toutiao_card_params={\"external_url\":\"https://h5.junjue888.com/h5/index.html?id=9467741690971614777&pid=103044&adP={\"aadd\":\"ddaa\"}\",\"name\":\"\"}&ad_params={\"id\":\"001\"}&toutiao_param={\"nno\":\"asdad\"}&shuntPid=1234`;
+console.time()
+const res = getUrlParam('',url);
+console.log(res);
+console.timeEnd()
