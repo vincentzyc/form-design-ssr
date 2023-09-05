e2742efe9f451b288b8da93b97e351682219deb3
vincentzyc <vincentzyc@qq.com>
2023-09-05 09:05:59 +0800

    🎉 init: init

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,41 @@
 <template>
   <div class="wrapper" :style="formatStyle(widgetsData && widgetsData.style)">
     <RenderPage :wgList="wgList" />
-    <!-- <p>测试服务端获取页面数据</p> -->
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 // import widgetsData from "@/data/pgdata";
-import { formatStyle } from "@/utils/format/unit";
-import RenderPage from "@cp/RenderPage.vue";
-import { defineComponent } from "vue";
-import { usePageContext } from "../../renderer/usePageContext";
+import { formatStyle } from '@/utils/format/unit';
+import RenderPage from '@/components/RenderPage.vue';
 
-export default defineComponent({
-  components: { RenderPage },
-  setup() {
-    const pageContext = usePageContext();
-    
-    const pageProps = pageContext.pageProps;
+let widgetsData = ref<Record<string, any> | null>(null);
+let wgList = ref<Record<string, any>[]>([]);
 
-    let widgetsData = $ref<Record<string, any> | null>(null);
-    let wgList = $ref<Record<string, any>[]>([]);
+// async function lazy(time=100) {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(true);
+//     }, time);
+//   });
+// }
 
-    if (pageProps) widgetsData = pageProps.pgData;
-    if (widgetsData) wgList = widgetsData.list;
+async function getPageData() {
+  // await lazy(3000);
+  const pgdata = await import('@/data/pgdata');
+  if (pgdata) widgetsData.value = pgdata.default || pgdata;
+  if (widgetsData.value) {
+    wgList.value = widgetsData.value.list;
+    useHead({
+      title: widgetsData.value.title,
+      htmlAttrs: {
+        lang: 'zh',
+      },
+    });
+  }
+}
 
-    // async function getPageData() {
-    //   // await lazy(300);
-    //   const pgdata = await import("@/data/pgdata");
-    //   if (pgdata) widgetsData = pgdata.default || pgdata;
-    //   if (widgetsData) wgList = widgetsData.list;
-    // }
+getPageData();
 
-    // await getPageData();
-
-    // console.log(wgList);
-
-    return {
-      widgetsData,
-      wgList,
-      formatStyle,
-    };
-  },
-});
+// console.log(wgList);
 </script>
