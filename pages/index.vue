a11d709424227087cffc61e32f75bf35f3475e60
vincentzyc <vincentzyc@qq.com>
2023-09-06 10:05:28 +0800

    🎉 init: init

--- pages/index.vue
+++ pages/index.vue
@@ -20,22 +20,30 @@ let wgList = ref<Record<string, any>[]>([]);
 //   });
 // }
 
-async function getPageData() {
-  // await lazy(3000);
-  const pgdata = await import('@/data/pgdata');
-  if (pgdata) widgetsData.value = pgdata.default || pgdata;
-  if (widgetsData.value) {
-    wgList.value = widgetsData.value.list;
-    useHead({
-      title: widgetsData.value.title,
+const PageData = await import('@/data/pgdata');
+
+function initHead(){
+  useHead({
+      title: widgetsData.value?.title,
       htmlAttrs: {
         lang: 'zh',
       },
+      script: [
+        {
+          children: `!function(e,t){function n(){var n=s.getBoundingClientRect().width;n>(t=t||540)&&(n=t);var i=100*n/e;s.style.fontSize=i+"px"}var i,o=document,d=window,s=o.documentElement;n(),d.addEventListener("resize",function(){clearTimeout(i),i=setTimeout(n,300)},!1),d.addEventListener("pageshow",function(e){e.persisted&&(clearTimeout(i),i=setTimeout(n,300))},!1),d.addEventListener("DOMContentLoaded",function(){o.body.style.position="relative",o.body.style.fontSize="14px",o.body.style.margin="0 auto",o.body.style.maxWidth=t+"px"},!1)}(750,640);`,
+        },
+      ],
     });
+}
+
+async function initPageData(pgdata: Record<string, any>) {
+  // await lazy(3000);
+  if (pgdata) widgetsData.value = pgdata.default || pgdata;
+  if (widgetsData.value) {
+    wgList.value = widgetsData.value.list;
+    initHead()
   }
 }
 
-getPageData();
-
-// console.log(wgList);
+initPageData(PageData);
 </script>
