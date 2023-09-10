5860ecefa105acc7af97058776a948a6b42545af
vincentzyc <vincentzyc@qq.com>
2023-09-10 23:09:34 +0800

    🎉init: init

---
+++ components/WidgetItems/WgInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="wrapClass" :style="item.style">
+    <div class="wg-title" v-show="item.showLabel" :style="{ width: changeRem(item.label.labelWidth) }">{{
+      item.label.labelTitle
+    }}</div>
+    <div class="flex-auto">
+      <textarea
+        v-if="item.isTextarea"
+        v-model="item.value"
+        :placeholder="item.placeholder"
+        class="wg-textarea"
+        :id="item.key"
+      />
+      <input
+        v-else
+        v-model="item.value"
+        :placeholder="item.placeholder"
+        class="wg-input"
+        :id="item.key"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { changeRem } from "@/utils/format/unit";
+// import { useWgFormList } from "@/composition/use-wgform";
+import { TypesInput } from "./WgTypes";
+
+const props = defineProps<{
+  item: TypesInput;
+}>();
+
+// const wgFormList = useWgFormList();
+// const { wgData, formData } = wgFormList.useAddForm(props.item);
+const wrapClass = ["wg-item", props.item.label.labelPosition === "top" ? "flex-column" : "align-middle"];
+</script>
