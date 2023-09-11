155b6590a8846bc436ab58ef6a7d99c896439dc7
vincentzyc <vincentzyc@qq.com>
2023-09-11 09:26:45 +0800

    🎉init: init

---
+++ components/WidgetItems/WgSplitLine.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="wg-splitLine wg-padding">
+    <hr class="splitLine-hr" :style="item.style" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { TypesSplitLine } from "./WgTypes";
+
+defineProps<{
+  item: TypesSplitLine;
+}>();
+</script>
