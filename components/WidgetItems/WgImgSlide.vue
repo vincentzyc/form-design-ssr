0f552012392e76a25371b0462b87031902456127
vincentzyc <vincentzyc@qq.com>
2023-09-18 13:58:39 +0800

    🎉init: init

---
+++ components/WidgetItems/WgImgSlide.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wg-imgslide">
+    <div :style="item.style">
+      <Swipe :autoplay="item.interval" :style="slideContentStyle">
+        <SwipeItem v-for="v in item.value">
+          <a :href="v.url">
+            <img :src="v.image" :style="{ width: '100%', height: '100%' }" />
+          </a>
+        </SwipeItem>
+      </Swipe>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { changeRem } from "@/utils/format/unit";
+import { Swipe, SwipeItem } from "vant";
+import { TypesImgSlide } from "./WgTypes";
+
+const props = defineProps<{
+  item: TypesImgSlide;
+}>();
+const slideContentStyle = { width: "100%", maxWidth: "640px", height: changeRem(props.item.style.height + "px") };
+</script>
