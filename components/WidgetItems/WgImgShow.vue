0b78a66ea0c1daebf7775db41fab4fadca31795c
vincentzyc <vincentzyc@qq.com>
2023-09-20 09:31:22 +0800

    🎉init: init

---
+++ components/WidgetItems/WgImgShow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wg-img-show" :style="formatStyle(item.style)">
+    <ul class="flex flex-wrap" v-for="imgitem in item.imglist">
+      <li :class="[item.styleType === 'col1' ? 'col-12' : 'col-6']">
+        <div :class="imgWrapClass(imgitem.link)" @click="jumpLink(imgitem.link)">
+          <img alt="图片" style="width: 100%" :src="imgitem.img" />
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script lang="ts" setup>
+import { formatStyle } from "@/utils/format/unit";
+import { isLink, jumpLink } from "@/utils/validate/link";
+import { TypesImgShow } from "./WgTypes";
+
+defineProps<{
+  item: TypesImgShow;
+}>();
+const imgWrapClass = (link: string) => ["flex", "flex-center", isLink(link) ? "cursor-pointer" : ""];
+</script>
