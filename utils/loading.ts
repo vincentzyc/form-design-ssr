deea992925a6bdedbf53daff971030fcbc0e62fc
vincentzyc <vincentzyc@qq.com>
2023-09-22 11:21:07 +0800

    🎉init: init

---
+++ utils/loading.ts
@@ -0,0 +1,12 @@
+export function openLoading(text: string): void {
+  showLoadingToast({
+    message: text,
+    duration: 0,       // 持续展示 toast
+    forbidClick: true, // 禁用背景点击
+    overlay: true,     //显示背景遮罩层
+  });
+}
+
+export function closeLoading(): void {
+  closeToast();
+}
