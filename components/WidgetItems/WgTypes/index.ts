0a57ec8c3b66c205e44be8e0c9f950a957ca1b49
vincentzyc <vincentzyc@qq.com>
2023-09-22 11:21:26 +0800

    🎉init: init

--- components/WidgetItems/WgTypes/index.ts
+++ components/WidgetItems/WgTypes/index.ts
@@ -1,6 +1,6 @@
 export * from "./StaticText"
 // export * from "./VideoPlay"
-// export * from "./Button"
+export * from "./Button"
 // export * from "./Tabs"
 // export * from "./Wechat"
 export * from "./ImgShow"
