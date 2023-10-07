1e627bc6843889dbb0fe5a65fa010493520fb35d
vincentzyc <vincentzyc@qq.com>
2023-10-07 14:09:10 +0800

    🎉 init: init

--- components/WidgetItems/WgImgShow.vue
+++ components/WidgetItems/WgImgShow.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="wg-img-show" :style="formatStyle(item.style)">
-    <ul class="flex flex-wrap" v-for="imgitem in item.imglist">
-      <li :class="[item.styleType === 'col1' ? 'col-12' : 'col-6']">
+  <div class="wg-img-show" :style="formatStyle(props.item.style)">
+    <ul class="flex flex-wrap" v-for="imgitem in props.item.imglist">
+      <li :class="[props.item.styleType === 'col1' ? 'col-12' : 'col-6']">
         <div :class="imgWrapClass(imgitem.link)" @click="jumpLink(imgitem.link)">
           <img alt="图片" style="width: 100%" :src="imgitem.img" />
         </div>
@@ -14,7 +14,7 @@ import { formatStyle } from "@/utils/format/unit";
 import { isLink, jumpLink } from "@/utils/validate/link";
 import { TypesImgShow } from "./WgTypes";
 
-defineProps<{
+const props = defineProps<{
   item: TypesImgShow;
 }>();
 const imgWrapClass = (link: string) => ["flex", "flex-center", isLink(link) ? "cursor-pointer" : ""];
