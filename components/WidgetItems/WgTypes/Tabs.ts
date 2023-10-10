e29f65b4b293512ae82f7fb2f37241be306285dd
vincentzyc <vincentzyc@qq.com>
2023-10-10 09:22:46 +0800

    🎉init: init

---
+++ components/WidgetItems/WgTypes/Tabs.ts
@@ -0,0 +1,15 @@
+export interface TypesTabs {
+  key: string,
+  type: string, // Tabs
+  name: string, // Tabs标签
+  value: number, // 0
+  activeTab: number, // 0
+  wgClassName: string, // "wg-formlist widget-active",
+  list: {
+    title: string, // 标签一
+    list: Record<any, any>[]  // []
+  }[],
+  style: {
+    margin: string // 0px 0px 0px 0px
+  }
+}
