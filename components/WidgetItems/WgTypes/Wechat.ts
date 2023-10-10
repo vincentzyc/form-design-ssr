5afc133f50bf1bac63b494da91b6d122b387c5bc
vincentzyc <vincentzyc@qq.com>
2023-10-10 09:22:54 +0800

    🎉init: init

---
+++ components/WidgetItems/WgTypes/Wechat.ts
@@ -0,0 +1,31 @@
+export interface TypesWechat {
+  key: string,
+  type: string, // "Wechat",
+  name: string, // "微信关注",
+  value: string, // "微信账号",
+  wechatName: string, // "",
+  btnText: string, // "+  关注",
+  logo: string, // "",
+  showName: boolean, // true,
+  showLogo: boolean, // true,
+  backgroundColor: string, // "",
+  positionFixed: 'auto' | 'top' | 'bottom',
+  fixedTypes: string[], // ['auto', 'top', 'bottom'] auto--正常 top--顶部悬浮  custom--自定义位置悬浮  bottom--底部悬浮
+  scrollHeight: number, // 0,
+  style: {
+    isImgBtn: boolean, // false,
+    value: string, // "",
+    margin: string, // "0px 0px 0px 0px",
+    color: string, // "#333",
+    fontsize: number, // 14,
+    fontSize: string, // "14px",
+    btnStyle: {
+      borderRadius: string, // '20px',
+      borderradius: number, // 20,
+      backgroundColor: string, // "#01cd0d",
+      color: string, // "#fff",
+      fontsize: number, // 14,
+      fontSize: string, // "14px"
+    }
+  }
+}
