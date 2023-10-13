0a1ed5f95ca4a47bb47286c205e268f1523b8544
vincentzyc <vincentzyc@qq.com>
2023-10-13 14:14:38 +0800

    🎉init: init

---
+++ components/WidgetItems/WgAgreement/AgreementPopup.vue
@@ -0,0 +1,31 @@
+<template>
+  <Popup v-model:show="showAgreement" class="agreement-popup" style="max-width: 576px">
+    <div class="agreement-wrap">
+      <iframe v-if="isLink(showText)" :src="showText" frameborder="0" style="width: 100%; height: 100%"></iframe>
+      <p v-else class="agreement-text" v-html="textBr(showText)"></p>
+    </div>
+    <div class="agreement-confirm" @click="close">我知道了</div>
+  </Popup>
+</template>
+
+<script lang="ts" setup>
+// 弹框组件（支持iframe嵌入页面）
+import { Popup } from "vant";
+import { textBr } from "@/utils/format/string";
+import { isLink } from "@/utils/validate/link";
+
+const emit = defineEmits(["close"]);
+
+let showAgreement = $ref(false);
+let showText = $ref("");
+
+const open = (text: string) => {
+  showText = text || "";
+  showAgreement = true;
+};
+const close = () => {
+  showAgreement = false;
+  emit("close");
+};
+defineExpose({ open, close });
+</script>
---
+++ components/WidgetItems/WgAgreement/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wg-agreement clearfix wg-padding" :id="props.item.key">
+    <Checkbox v-model="formData[props.item.apiKey]" :style="formatStyle(props.item.style)" shape="square">
+      <p :style="{ margin: 0, color: props.item.style.color }">
+        <span>{{ props.item.tipText }}</span>
+        <span v-for="(titleText, key) in props.item.titleTexts">
+          <span v-if="key > 0">和</span>
+          <span
+            v-if="titleText.title"
+            :style="{ color: props.item.agreementColor }"
+            @click="openAgreement(titleText.text, $event)"
+            >{{ titleText.title }}</span
+          >
+        </span>
+      </p>
+    </Checkbox>
+    <AgreementPopup ref="elAgreement" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { formatStyle } from "@/utils/format/unit";
+import { Checkbox } from "vant";
+import AgreementPopup from "./AgreementPopup.vue";
+// import { useWgFormList } from "@/composition/use-wgform";
+import { TypesAgreement } from "../WgTypes";
+
+const props = defineProps<{
+  item: TypesAgreement;
+}>();
+
+const elAgreement = ref<typeof AgreementPopup>();
+// const wgFormList = useWgFormList();
+// const { wgData, formData } = wgFormList.useAddForm(props.item);
+
+const openAgreement = (text: string, event: MouseEvent) => {
+  event.stopPropagation();
+  elAgreement.value && elAgreement.value.open(text);
+};
+</script>
