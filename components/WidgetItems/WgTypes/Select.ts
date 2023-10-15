8662e8064c3df92d608abec1c53e63f4ad818de6
vincentzyc <vincentzyc@qq.com>
2023-10-15 14:02:55 +0800

    🎉init: init

--- components/WidgetItems/WgTypes/Select.ts
+++ components/WidgetItems/WgTypes/Select.ts
@@ -1,20 +1,20 @@
 export interface TypesSelect {
-  key: string,
-  type: string, // "Select",
-  name: string, //  "下拉选择框",
-  value: string, //  "",
-  apiKey: string, //  "educationLevel",
-  placeholder: string, //  "请选择教育程度",
-  showLabel: boolean, //  true,
+  key: string;
+  type: string; // "Select",
+  name: string; //  "下拉选择框",
+  value: string; //  "",
+  apiKey: string; //  "educationLevel",
+  placeholder: string; //  "请选择教育程度",
+  showLabel: boolean; //  true,
   label: {
-    labelTitle: string, //  "教育程度",
-    labelPosition: "left" | "top",
-    labelwidth: number, //  66,
-    labelWidth: string, //  "66px"
-  },
-  fieldTypes: string, //  "selectTypes",
-  options: string[], // ["硕士及以上","本科","大专","中专/高中及以下"],
+    labelTitle: string; //  "教育程度",
+    labelPosition: 'left' | 'top';
+    labelwidth: number; //  66,
+    labelWidth: string; //  "66px"
+  };
+  fieldTypes: string; //  "selectTypes",
+  options: { text: string; value: string }[]; // [{ text: '硕士及以上', value: '硕士及以上' },{ text: '本科', value: '本科' }],
   style: {
-    margin: string, //  "0px 0px 0px 0px"
-  }
+    margin: string; //  "0px 0px 0px 0px"
+  };
 }
