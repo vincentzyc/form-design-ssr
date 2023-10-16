e139af86fa87234f29b7059c0d5d82d7673e6b58
vincentzyc <vincentzyc@qq.com>
2023-10-16 09:27:12 +0800

    🎉init: init

---
+++ components/WidgetItems/WgSelect.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class="wrapClass" :style="props.item.style">
+    <div class="wg-title" v-show="props.item.showLabel" :style="{ width: changeRem(props.item.label.labelWidth) }">{{
+      props.item.label.labelTitle
+    }}</div>
+    <div class="flex-auto disabled-input" @click="openPicker">
+      <input
+        readonly
+        :id="props.item.key"
+        v-model="props.item.value"
+        :placeholder="props.item.placeholder"
+        class="wg-input"
+      />
+    </div>
+    <VanPopup v-model:show="showPicker" round position="bottom">
+      <VanPicker show-toolbar :columns="props.item.options" @cancel="closePicker" @confirm="onConfirm" />
+    </VanPopup>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PickerConfirmEventParams } from 'vant';
+import { changeRem } from '@/utils/format/unit';
+// import { useWgFormList } from "@/composition/use-wgform";
+import { TypesSelect } from './WgTypes';
+
+const props = defineProps<{
+  item: TypesSelect;
+}>();
+// const wgFormList = useWgFormList();
+// const { wgData, formData } = wgFormList.useAddForm(props.item);
+let showPicker = ref(false);
+const wrapClass = ['wg-item', props.item.label.labelPosition === 'top' ? 'flex-column' : 'align-middle'];
+
+const openPicker = () => {
+  showPicker.value = true;
+};
+const closePicker = () => {
+  showPicker.value = false;
+};
+const onConfirm = (value: PickerConfirmEventParams) => {
+  console.log(value);
+  props.item.value = value.selectedValues[0] as string;
+  closePicker();
+};
+</script>
