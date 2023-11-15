165cd3b926395c09806565f83724aa8441742969
vincentzyc <vincentzyc@qq.com>
2023-11-15 17:17:21 +0800

    🎉init: init

--- pages/index.vue
+++ pages/index.vue
@@ -7,41 +7,48 @@
 <script setup lang="ts">
 import { formatStyle } from '@/utils/format/unit';
 import RenderPage from '@/components/RenderPage.vue';
+import pgdata from '@/data/pgdata';
 
 let widgetsData = ref<Record<string, any> | null>(null);
 let wgList = ref<Record<string, any>[]>([]);
 
-async function lazy(time=100) {
+// async function lazy(time = 100) {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(true);
+//     }, time);
+//   });
+// }
+
+function getPageData() {
   return new Promise(resolve => {
     setTimeout(() => {
-      resolve(true);
-    }, time);
+      resolve(pgdata);
+    }, 1000);
   });
 }
+// 模拟接口请求
+const PageData = await getPageData();
 
- // 模拟接口请求
-await lazy(300);
-const PageData = await import('@/data/pgdata');
-
-function initHead(){
+function initHead() {
   useHead({
-      title: widgetsData.value?.title,
-      htmlAttrs: {
-        lang: 'zh',
+    title: widgetsData.value?.title,
+    htmlAttrs: {
+      lang: 'zh',
+    },
+    script: [
+      {
+        children: `!function(e,t){function n(){var n=s.getBoundingClientRect().width;n>(t=t||540)&&(n=t);var i=100*n/e;s.style.fontSize=i+"px"}var i,o=document,d=window,s=o.documentElement;n(),d.addEventListener("resize",function(){clearTimeout(i),i=setTimeout(n,300)},!1),d.addEventListener("pageshow",function(e){e.persisted&&(clearTimeout(i),i=setTimeout(n,300))},!1),d.addEventListener("DOMContentLoaded",function(){o.body.style.position="relative",o.body.style.fontSize="14px",o.body.style.margin="0 auto",o.body.style.maxWidth=t+"px"},!1)}(750,640);`,
       },
-      script: [
-        {
-          children: `!function(e,t){function n(){var n=s.getBoundingClientRect().width;n>(t=t||540)&&(n=t);var i=100*n/e;s.style.fontSize=i+"px"}var i,o=document,d=window,s=o.documentElement;n(),d.addEventListener("resize",function(){clearTimeout(i),i=setTimeout(n,300)},!1),d.addEventListener("pageshow",function(e){e.persisted&&(clearTimeout(i),i=setTimeout(n,300))},!1),d.addEventListener("DOMContentLoaded",function(){o.body.style.position="relative",o.body.style.fontSize="14px",o.body.style.margin="0 auto",o.body.style.maxWidth=t+"px"},!1)}(750,640);`,
-        },
-      ],
-    });
+    ],
+  });
 }
 
 async function initPageData(pgdata: Record<string, any>) {
   if (pgdata) widgetsData.value = pgdata.default || pgdata;
   if (widgetsData.value) {
     wgList.value = widgetsData.value.list;
-    initHead()
+    initHead();
   }
 }
 
