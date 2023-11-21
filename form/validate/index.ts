fb13fdfd0c1bcbc4fc90365683781859109c2105
vincentzyc <vincentzyc@qq.com>
2023-11-21 10:40:06 +0800

    🎉init: init

--- form/validate/index.ts
+++ form/validate/index.ts
@@ -1,8 +1,8 @@
 import { scrollIntoView } from '@/utils/dom';
 
 interface TypeRuleList {
-  [key: string]: (...args: any[]) => true | string
-};
+  [key: string]: (...args: any[]) => true | string;
+}
 // interface TypeSubmitAgrs {
 //   formData: Record<string, any>,
 //   wgForms: Record<string, any>[],
@@ -13,24 +13,25 @@ const ruleList: TypeRuleList = {
   phone: (value: string) => {
     if (!value) return '请输入手机号';
     if (/^1[0-9]{10}$/.test(value)) return true;
-    return '手机号码不正确'
+    return '手机号码不正确';
   },
   verifyCode: (value: string) => {
     if (!value) return '请输入验证码';
     if (value.length === 6) return true;
-    return "验证码错误";
+    return '验证码错误';
   },
   name: (value: string) => {
     if (!value) return '请输入姓名';
     if (/^[\u4e00-\u9fa5]{2,20}$/.test(value)) return true;
     if (value.length < 2 || value.length > 20) return '姓名长度不能小于2或超过20';
-    return '姓名必须为汉字'
+    return '姓名必须为汉字';
   },
   mail: (value: string) => {
     if (!value) return '请输入邮箱';
-    const emailReg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const emailReg =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (emailReg.test(value)) return true;
-    return '邮箱格式不正确'
+    return '邮箱格式不正确';
   },
   shootPlace: (value: string[]) => {
     if (value.length === 0) return '请选择旅拍城市';
@@ -74,8 +75,13 @@ const ruleList: TypeRuleList = {
   },
   idCard: (value: string) => {
     if (!value) return '请输入身份证号';
-    if (/(^\d{8}(0\d|10|11|12)([0-2]\d|30|31)\d{3}$)|(^\d{6}(18|19|20)\d{2}(0\d|10|11|12)([0-2]\d|30|31)\d{3}(\d|X|x)$)/.test(value)) return true;
-    return '请输入正确的身份证号码'
+    if (
+      /(^\d{8}(0\d|10|11|12)([0-2]\d|30|31)\d{3}$)|(^\d{6}(18|19|20)\d{2}(0\d|10|11|12)([0-2]\d|30|31)\d{3}(\d|X|x)$)/.test(
+        value
+      )
+    )
+      return true;
+    return '请输入正确的身份证号码';
   },
   city: (value: string[]) => {
     if (!value) return '请选择城市';
@@ -88,39 +94,71 @@ const ruleList: TypeRuleList = {
   randomCode: (value: string, item: Record<string, any>) => {
     if (!value) return '请输入验证码';
     if (item.value.toUpperCase() !== item.codeValue.toUpperCase()) {
-      item.getCode()
+      item.getCode();
       return '验证码错误';
     }
-    return true
-  }
-}
+    return true;
+  },
+};
 
-export function valiDate(obj: Record<string, any>): boolean | string {
-  if (typeof (obj.isRequired) !== 'boolean') obj.isRequired = true; //默认必填
+export function valiDate(obj: Record<string, any>): true | string {
+  if (typeof obj.isRequired !== 'boolean') obj.isRequired = true; //默认必填
   if (obj.isRequired && obj.value === '') return obj.placeholder || '请完善信息';
   if (!ruleList[obj.apiKey]) return true;
   if (obj.apiKey === 'phone') {
     if (ruleList[obj.apiKey](obj.value) === true) {
-      return obj.showCode ? ruleList[obj.codeKey](obj.codeValue) : true
+      return obj.showCode ? ruleList[obj.codeKey](obj.codeValue) : true;
     }
   }
   if (obj.isRequired === false && (obj.value === '' || obj.value === false)) return true;
-  return ruleList[obj.apiKey](obj.value, obj)
+  return ruleList[obj.apiKey](obj.value, obj);
+}
+
+export function valiAllDate(
+  list: Record<string, any>[] = [],
+  formData: Record<string, any> = {},
+  isCcrollIntoView = true
+): Record<string, any> | string {
+  for (const item of list) {
+    if (Array.isArray(item.list) && item.list.length > 0) {
+      let chlidRes = valiAllDate(item.list, formData, isCcrollIntoView);
+      if (typeof chlidRes === 'string') return chlidRes;
+    }
+    if (!item.apiKey) continue;
+    let res = valiDate(item);
+    if (res === true) {
+      formatParam(item, formData);
+      continue;
+    }
+    if (isCcrollIntoView) {
+      const dom = document.getElementById(item.key);
+      if (dom) scrollIntoView(dom);
+    }
+    return res;
+  }
+  return formData;
+}
+function formatParam(item: Record<string, any>, formData: Record<string, any>) {
+  if (!Object.prototype.hasOwnProperty.call(item, 'apiKey')) return;
+  if (item.type === 'phone' && item.showCode) {
+    formData[item.codeKey] = item.codeValue;
+  }
+  formData[item.apiKey] = item.value;
 }
 
 export function valiWgValue(formData: Record<string, any>, wgForms: Record<string, any>[], isScrollIntoView = true) {
   for (const wg of wgForms) {
-    if (Object.prototype.hasOwnProperty.call(formData, wg.apiKey)) wg.value = formData[wg.apiKey]
-    if (Object.prototype.hasOwnProperty.call(formData, wg.codeKey)) wg.codeValue = formData[wg.codeKey]
+    if (Object.prototype.hasOwnProperty.call(formData, wg.apiKey)) wg.value = formData[wg.apiKey];
+    if (Object.prototype.hasOwnProperty.call(formData, wg.codeKey)) wg.codeValue = formData[wg.codeKey];
     const res = valiDate(wg);
     if (res === true) {
-      continue
+      continue;
     }
     if (isScrollIntoView) {
-      const dom = document.getElementById(wg.key)
-      if (dom) scrollIntoView(dom)
+      const dom = document.getElementById(wg.key);
+      if (dom) scrollIntoView(dom);
     }
     return res;
   }
-  return true
+  return true;
 }
