479f08eb8843e05c9b3752cf8236f8c4ffae0c46
vincentzyc <vincentzyc@qq.com>
2024-02-05 18:30:59 +0800

    🎉init: init

---
+++ server/api/hello.ts
@@ -0,0 +1,10 @@
+import pgdata from '@/data/pgdata';
+
+export default defineEventHandler(event => {
+  // 模拟接口返回
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(pgdata);
+    }, 300);
+  });
+});
