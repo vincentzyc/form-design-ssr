f099b11142e1d365198f161068d5be9161803956
vincentzyc <vincentzyc@qq.com>
2024-02-12 11:25:22 +0800

    🎉init: init

--- server/api/page.ts
+++ server/api/page.ts
@@ -2,7 +2,6 @@ import pgdata from '@/data/pgdata';
 
 export default defineEventHandler(event => {
   console.log(getRequestURL(event));
-  console.log(getRouterParam(event,'name'))
   // 模拟接口返回
   return new Promise(resolve => {
     setTimeout(() => {
